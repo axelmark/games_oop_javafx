6d1a11472ffcb0ca9d63696ba33313d33f0380e4
axelmark <axelmark@mail.ru>
2022-01-05 11:42:14 +0300

    2. Каркас шахматной доски [#792]

--- chess/src/main/java/ru/job4j/chess/Logic.java
+++ chess/src/main/java/ru/job4j/chess/Logic.java
@@ -22,9 +22,9 @@ public final class Logic {
     }
 
     private boolean free(Cell[] steps) throws OccupiedCellException {
-        for (int i = 0; i < steps.length; i++) {
-            for (int j = 0; j < figures.length; j++) {
-                if (figures[j].position().equals(steps[i])) {
+        for (Cell step : steps) {
+            for (Figure figure : figures) {
+                if (figure.position().equals(step)) {
                     throw new OccupiedCellException();
                 }
             }
